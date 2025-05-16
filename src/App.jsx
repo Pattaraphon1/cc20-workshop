54565469bdc213d6cfc6cbc96b3d22e32eda330e
pattaraphon1 <klgsa51@gmail.com>
2025-05-16 15:36:23 +0700

    addtocart no duplicate

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,16 @@ function App() {
   },[])
 
   const addToCart = (id,  title, price ) => {
-    let newItem = {id:id, title:title, price:price, quantity:1}
-    setCarts([...carts, newItem])
+    let idx = carts.findIndex(el=> el.id === id) // not found -1
+    let newItem
+    if(idx === -1){
+     newItem = {id:id, title:title, price:price, quantity:1}
+     setCarts([...carts, newItem])
+    }else {
+     const clonedCart = [...carts]
+     clonedCart[idx].quantity += 1
+     setCarts(clonedCart)
+    }
   }
 
   return (
